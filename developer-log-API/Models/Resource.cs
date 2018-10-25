a57a0842d1adba9bad5647a836e050bbcb2155fd
Philip Patton <ppatton35745@gmail.com>
2018-10-25 13:31:41 -0500

    added resource and topic model

---
+++ developer-log-API/Models/Resource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace developer_log_API.Models
+{
+    public class Resource
+    {
+        [Key]
+        public int ResourceID { get; set; }
+
+        [Required]
+        public int UserID { get; set; }
+
+        [Required]
+        public int ResourceTypeID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+    }
+}
+
---
+++ developer-log-API/Models/Topic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace developer_log_API.Models
+{
+    public class Topic
+    {
+        [Key]
+        public int TopicID { get; set; }
+
+        [Required]
+        public int UserID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+    }
+}
